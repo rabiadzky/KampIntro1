de5829ca80a518233de10fcd767abc384475bc6a
rabia.dzky@hotmail.com <rabia.dzky@hotmail.com>
2021-01-19 18:52:41 +0300

    Proje dosyası ekle.

---
+++ KampIntro1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace KampIntro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapilmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if(dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapilmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+        }
+    }
+}
